18d13119e8d6c48a783f53896dd71dbd3b4b51ac
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-02-12 18:39:44 +0600

    3rd commit

--- main/qa/views.py
+++ main/qa/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Question
+from django.core.paginator import Paginator
 
 def home(request):
-    return render(request,'home.html')
+    quest = Question.objects.all().order_by('-id')
+    paginator = Paginator(quest, 1)
+    page_num = request.GET.get('page', 1)
+    quest = paginator.page(page_num)
+    return render(request, 'home.html', {'quest': quest})
