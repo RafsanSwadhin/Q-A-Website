da0e7186de3b6e21ed56e3537b361d0e4ecca32e
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-02-12 22:10:16 +0600

    detail.html

--- main/qa/views.py
+++ main/qa/views.py
@@ -3,8 +3,17 @@ from .models import Question
 from django.core.paginator import Paginator
 
 def home(request):
-    quest = Question.objects.all().order_by('-id')
+    if "qu" in request.GET:
+        qu = request.GET['qu']
+        quest = Question.objects.filter(title__icontains=qu).order_by('-id')
+    else:
+        quest = Question.objects.all().order_by('-id')
     paginator = Paginator(quest, 1)
     page_num = request.GET.get('page', 1)
     quest = paginator.page(page_num)
     return render(request, 'home.html', {'quest': quest})
+
+def detail(request,id):
+    quest = Question.objects.all()
+    return render(request,'detail.html',{'quest':quest})
+    
