964288aa6e45fe60e2f703cd183524f74ebbf180
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-02-18 17:35:42 +0600

    new

---
+++ questans/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Question
+from django.core.paginator import Paginator
+def home(request):
+    if "q" in request.GET:
+        q = request.GET['q']
+        quests = Question.objects.filter(title__icontains=q).order_by('-id')
+    else:
+        quests = Question.objects.all().order_by('-id')
+    paginator = Paginator(quests,2)
+    page_num = request.GET.get('page',1)
+    quests = paginator.page(page_num)
+    return render(request,'home.html',{'quests':quests})
